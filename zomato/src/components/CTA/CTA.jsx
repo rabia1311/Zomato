b52a8a560c3597fee96b20ebc3259d6598047d2e
rabia1311 <rabiasultana.n@gmail.com>
2023-05-26 17:25:59 +1030

    CTA COMPLETED

--- zomato/src/components/CTA/CTA.jsx
+++ zomato/src/components/CTA/CTA.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../CTA/CTA.scss";
-import Iphone from "../../images/Iphone.png"
+import phonpic from "../../images/phonpic.png"
 import Google from "../../images/Google.png"
 import AppStore from "../../images/AppStore.png"
 const CTA = () => {
+    const[email,setEmail]=useState(true);
+    const[phone,setPhone]=useState(false);
+
   return (
     <div className="cta">
         <div className="left">
-<img src={Iphone} alt="phone" />
+<img src={phonpic} alt="phone" />
         </div>
         <div className="right">
             <h1>Get the Zomato app </h1>
@@ -15,31 +18,34 @@ const CTA = () => {
             </p>
 <div className="radio">
     <div className="radioButn">
-        <input type="radio" name="email"/>
+        <input type="radio" name="radio" id="email"  onClick={()=>setEmail(true)||setPhone(false)}/>
         <label htmlFor="email">Email</label>
 
     </div>
     <div className="radio">
     <div className="radioButn">
-        <input type="radio" name="phone"/>
+        <input type="radio" name="radio" id="phone" onClick={()=>setPhone(true)||setEmail(false)}/>
         <label htmlFor="phone">Phone</label>
 
     </div>
+    </div>
 </div>
+<br/>
 <form className="input">
-    <input type="email" placeholder="Email"/>
-    <input type="number" placeholder="Phone"/>
+    {email && < input type="email" placeholder="Email"/>}
+    {phone &&<input type="number" placeholder="Phone"/>}
     <button type ="submit">Share App Link</button>
     </form>
     <span>Download App from </span>
   
     <div className="img">
         <img src={Google} alt="google" />
+        <br/>
         <img src={AppStore} alt="app" />
     </div>
 
 
-        </div>
+       
     </div>
    </div>
   )
